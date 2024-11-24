89701a19a74693b50d87919aac8f53f1d5f253f6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-24 13:07:51 +0530

    Update signup_login.js

--- signup_login.js
+++ signup_login.js
@@ -19,15 +19,40 @@ document.getElementById("signupForm").onsubmit = function (e) {
   const Username = document.getElementById("signupusername").value;
   const Password = document.getElementById("signuppass").value;
   const Rpassword = document.getElementById("signuprpass").value;
+
   if (Password !== Rpassword) {
     alert("Repeat password is incorrect");
   } else if (Username.length <= 5 || Name.length <= 3) {
-    alert("Username too small");
+    alert("Username or Name too short");
   } else if (Password.length <= 8) {
-    alert("Password too small");
+    alert("Password too short");
   } else {
-    alert(Name);
-    closeModal("signupModal");
+    const url = "https://linkup-backend-production.up.railway.app/signup/";
+    const data = {
+      name: Name,
+      username: Username,
+      password: Password,
+    };
+    const headers = { "Content-Type": "application/json" };
+
+    fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          alert("Signup successful!" + data.success);
+          closeModal("signupModal");
+        } else {
+          alert("Signup failed: " + (data.message || "Unknown error"));
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred while signing up. Please try again later.");
+      });
   }
 };
 
