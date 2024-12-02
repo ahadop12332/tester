b56931642729628a953430fe6a9ad9c11aafda4f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-02 17:48:12 +0530

    Update check_user_logged.js

--- js/check_user_logged.js
+++ js/check_user_logged.js
@@ -1,3 +1,20 @@
 hm = document.getElementById('change_txt')
-let cookies = document.cookie;
-hm.textContent = cookies
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
+
+const session = getCookie('session');
+if (session == null) {
+  hm.textContent = "Can't find any sessions"
+} else {
+  hm.textContent = session
+}
+
