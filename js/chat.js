d31b17282a7f3e7fbd8ec6b4697590f89c5a465e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-05 16:31:01 +0530

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,11 +1,47 @@
 var chats = [];
 
-import { check_session, getCookie } from "./check_user_logged.js";
+function getCooke(name) {
+  const cookies = document.cookie.split('; ');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
+
+async function check_sessin() {
+  const session = getCooke('session');
+  if (session == null) {
+    return "Unmatched";
+  } else {
+    try {
+      const data = { session: session };
+      const url = "https://linkup-backend-production.up.railway.app/check_session/";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      if (result.success && typeof result.success === "string" && result.success === "Same") {
+        return "redirect";
+      } else {
+        return "Unmatched";
+      }
+    } catch (err) {
+      console.log("Error: " + err.message);
+      return "Unmatched";
+    }
+  }
+}
 
 const chatlist_url = "https://linkup-backend-production.up.railway.app/chatlist/";
 const userinfo_url = "https://linkup-backend-production.up.railway.app/userinfo/";
-const session = getCookie('session');
-const check_session_status = check_session();
+const session = getCooke('session');
+const check_session_status = check_sessin();
 const chatlistFetch = {
   "session": session
 };
