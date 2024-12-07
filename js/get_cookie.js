0c90c3bb1f0c98936aa3afbd3973a3ca790a9b2a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-07 12:08:25 +0530

    Create get_cookie.js

---
+++ js/get_cookie.js
@@ -0,0 +1,10 @@
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split('=');
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
