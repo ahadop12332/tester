669c0d88f5990e560a7e92f3d472ee7e8c6f9627
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-09 17:03:23 +0530

    Update get_msgs.js

--- js/get_msgs.js
+++ js/get_msgs.js
@@ -1,5 +1,6 @@
-var loadChatWsClosed = false;
+var isChatWsOpen;
 var msgs;
+var mWs;
 
 async function get_msgs() {
   const ws_url = "wss://linkup-backend-production.up.railway.app/ws/loadMsg/";
@@ -31,14 +32,15 @@ async function get_msgs() {
     return;
   }
   
-  const ws = new WebSocket(ws_url);
-  ws.onopen = () => {
+  mWs = new WebSocket(ws_url);
+  mWs.onopen = () => {
     console.log('Message WebSocket connected');
-    ws.send(JSON.stringify({ session }));
+    mWs.send(JSON.stringify({ session }));
+    isChatWsOpen = true;
   };
 
   
-  ws.onmessage = (event) => {
+  mWs.onmessage = (event) => {
     msg = JSON.parse(event.data);
     if (msg.error && msg.error === "INVALID USER OR SESSION") {
       ws.close();
@@ -55,24 +57,22 @@ async function get_msgs() {
     }
   };
 
-  ws.onclose = () => {
+  mWs.onclose = () => {
     console.warn('Message WebSocket closed, reconnecting...');
-    loadChatWsClosed = true;
+    isChatWsOpen = false;
     setTimeout(() => get_msgs(), 600);
   };
 
-  ws.onerror = (error) => {
+  mWs.onerror = (error) => {
     console.error('Message WebSocket error:', error);
   };
+}
 
-  while (true) {
-    if (loadChatWsClosed) {
-      const chatId = chat.getAttribute("chat_id");
-      if (chatId !== '0') {
-        ws.send({'chat_id': String(chatId)});
-      }
-    } else {
-      break;
+async function getChatMsg() {
+  if (isChatWsOpen) {
+    const chatId = chat.getAttribute("chat_id");
+    if (chatId !== '0') {
+      mWs.send(JSON.stringify({ chat_id: String(chatId) }));
     }
   }
 }
