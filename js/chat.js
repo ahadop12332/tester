610a41f7855dabfb8cfdd8b81e710e4960cff892
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-10 17:51:38 +0530

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -35,7 +35,6 @@ async function get_chats() {
       let chatItems = '';
       document.querySelector('.loader').style.display = 'inline-block';
       for (let chat_id of data.chats) {
-        chatList.push(chat_id);
         try {
           const chatResponse = await fetch(userinfo_url, {
             method: "POST",
