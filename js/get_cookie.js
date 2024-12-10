65ffe4eb8cf06cd1b0e2a77b9a9e78dcb0fd0863
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-10 12:19:13 +0530

    Update get_cookie.js

--- js/get_cookie.js
+++ js/get_cookie.js
@@ -1,4 +1,4 @@
-function getCookie(name) {
+export function getCookie(name) {
   const cookies = document.cookie.split('; ');
   for (let cookie of cookies) {
     const [key, value] = cookie.split('=');
