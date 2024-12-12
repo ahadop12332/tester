07467886b1f77a2cd109cce8e833f8c139fadf43
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-12 14:22:55 +0530

    Update sendMessage.js

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -3,7 +3,21 @@ var isSendMsgWsOpen = false;
 
 async function sendMessage() {
   if (!isSendMsgWsOpen) {
-    const ws_url = "wss://linkup-backend-production.up.railway.app/ws/loadMsg/";
-    
+    const ws_url = "wss://linkup-backend-production.up.railway.app/ws/sendMessage/";
+    const session = getCookie('session');
+    if (!session) {
+      window.location.href = "/index.html";
+      return;
+    }
+    sWs = new WebSocket(ws_url);
+    sWs.onopen = () => {
+      console.log('Message WebSocket connected');
+      sWs.send(JSON.stringify({ session }));
+      isSendMsgWsOpen = true;
+    };
+
+    sWs.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+    };
   }
 }
