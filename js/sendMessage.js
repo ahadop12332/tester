7dd5c09617d1e3d5f3570aaadc3920fadbc3b03b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-12 14:16:09 +0530

    Create sendMessage.js

---
+++ js/sendMessage.js
@@ -0,0 +1,9 @@
+var sWs;
+var isSendMsgWsOpen = false;
+
+async function sendMessage() {
+  if (!isSendMsgWsOpen) {
+    const ws_url = "wss://linkup-backend-production.up.railway.app/ws/loadMsg/";
+    
+  }
+}
